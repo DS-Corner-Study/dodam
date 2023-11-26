88cf605471b8caf559d5a64d9543caccaf8063a3
soyoon1 <20210841@duksung.ac.kr>
2023-11-27 03:14:01 +0900

    [1127] 9주차 스터디 연관관계 매핑

---
+++ relationship/src/main/java/com/springboot/relationship/AdvancedJpaApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.relationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableJpaAuditing
+public class AdvancedJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedJpaApplication.class, args);
+	}
+
+}
